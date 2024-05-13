9e53cd372aa69f834f6afa15ed05a51c9201e904
eric-coutinho <eric.coutinhop@hotmail.com>
2024-05-13 11:11:19 -0300

    navbar broken on mobile

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -16,8 +16,8 @@ export default function NavBar() {
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse id="basic-navbar-nav" className={styles.navText}>
+          <Nav>
             <NavDropdown title="Localizações" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Curitiba</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
