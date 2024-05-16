234874f7c106614e1d97dad48a642530cdf6028a
eric-coutinho <eric.coutinhop@hotmail.com>
2024-05-16 11:05:14 -0300

    Login not working

---
+++ backend/src/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const UserController = require('../controller/userController');
+const route = express.Router();
+
+route
+    .post('/register', UserController.register)
+    .post('/login', UserController.login)
+    .delete('/delete/:id', UserController.delete)
+    .get('/find', UserController.findUser)
+
+module.exports = route;
