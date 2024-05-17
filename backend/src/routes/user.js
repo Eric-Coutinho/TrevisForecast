8ec9361ab0143405b2240eebdf76114137f9fc07
eric-coutinho <eric.coutinhop@hotmail.com>
2024-05-17 11:09:58 -0300

    Login almost working, need to save token on LocalStorage

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -4,7 +4,6 @@ const route = express.Router();
 
 route
     .post('/register', UserController.register)
-    .post('/register2', UserController.registerTeste)
     .post('/login', UserController.login)
     .delete('/delete/:id', UserController.delete)
     .get('/find', UserController.findUser)
